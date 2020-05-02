8cec99e220adcc2b957ff00c7cbe9562893f2b06
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2020-05-01 19:03:24 -0700

    Normalize semicolons.

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-var https = require('https')
+var https = require('https');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let dataLocation = "https://www.iwillfearnoevil.com/screen/string.txt";
   let targetIntegers;
   const externalRequest = https.request(dataLocation, externalResponse => {
     externalResponse.on('data', responseBuffer => {
-      let responseJson = Buffer.from(responseBuffer)
-      let rawIntegers = responseJson.toString('utf-8').split("\n")
-      let orderedIntegers = sortAndUniqify(rawIntegers)
-      let firstThree = [orderedIntegers[0], orderedIntegers[1], orderedIntegers[2]]
+      let responseJson = Buffer.from(responseBuffer);
+      let rawIntegers = responseJson.toString('utf-8').split("\n");
+      let orderedIntegers = sortAndUniqify(rawIntegers);
+      let firstThree = [orderedIntegers[0], orderedIntegers[1], orderedIntegers[2]];
       res.render('index', { targetIntegers: firstThree, dataLocation: dataLocation });
     });
   });
 
   externalRequest.on('error', error => {
-    console.error(error)
-  })
+    console.error(error);
+  });
 
-  externalRequest.end()
+  externalRequest.end();
 
   sortAndUniqify = (intArray) => {
-    let uniqueInts = {}
+    let uniqueInts = {};
     intArray.forEach(int => {
       // This leverages the nature of objects in JS, which will order and constrain unique keys
       let parsed = parseInt(int)
       uniqueInts[`${parsed}`] = parsed
-    })
-    return Object.values(uniqueInts)
-  }
+    });
+    return Object.values(uniqueInts);
+  };
 });
 
 module.exports = router;
